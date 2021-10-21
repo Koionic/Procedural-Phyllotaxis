3db2f001b15a9f80007cc21623fcf55db92ac236
Joshua <josh.bungarra@iinet.net.au>
2021-10-21 21:12:25 +0800

    Initial commit

---
+++ Procedural Phyllotaxis/Assets/Scripts/ObjectPhyllotaxis.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPhyllotaxis : MonoBehaviour
+{
+
+    public GameObject dot;
+    [Range(0f, 360f)]
+    public float degreeDelta;
+    public float scale;
+    public int currentIndex;
+    public float dotScale;
+
+    public int colourIndex;
+    public Color currentColour;
+    public List<Color> gradientColours = new List<Color>();
+
+    [Range(0f, 1f)]
+    public float gradientLerpRate;
+    
+    private Vector2 phyllotaxisPosition;
+
+    private List<Vector3> positions = new List<Vector3>();
+    private List<GameObject> dots = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClearPhyllyotaxis();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            CreateNewPhyllotaxisPoint();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ClearPhyllyotaxis();
+        }
+    }
+
+    /// <summary>
+    /// Calculates the position of the dot using the phyllotaxis formula
+    /// </summary>
+    /// <param name="inDegreeDelta"></param>
+    /// The change in angle per step in degrees
+    /// <param name="inScale"></param>
+    /// <param name="inCount"></param>
+    /// The iteration into the pattern
+    /// <returns></returns>
+    private Vector2 CalculatePhyllotaxis(float inDegreeDelta, float inScale, int inCount)
+    {
+        double angle = inCount * (inDegreeDelta * Mathf.Deg2Rad);
+
+        float r = inScale * Mathf.Sqrt(inCount);
+
+        //Calculating the coordinate of the new position
+        float x = r * (float) System.Math.Cos(angle);
+        float y = r * (float) System.Math.Sin(angle);
+
+        Vector2 vec2 = new Vector2(x, y);
+        
+        return vec2;
+    }
+    
+    private Vector2 CalculatePhyllotaxis(float inDegreeDelta, float inScale, int inCount, out float rotation)
+    {
+        double angle = inCount * (inDegreeDelta * Mathf.Deg2Rad);
+
+        rotation = (float)angle * Mathf.Rad2Deg;
+
+        float r = inScale * Mathf.Sqrt(inCount);
+
+        //Calculating the coordinate of the new position
+        float x = r * (float) System.Math.Cos(angle);
+        float y = r * (float) System.Math.Sin(angle);
+
+        Vector2 vec2 = new Vector2(x, y);
+        
+        return vec2;
+    }
+
+    void CreateNewPhyllotaxisPoint()
+    {
+        //Calculates 2D position for new dot
+        phyllotaxisPosition = CalculatePhyllotaxis(degreeDelta, scale, currentIndex, out float dotRotateZ);
+
+        //Creates and stores 3D position for new dot
+        positions.Add(new Vector3(phyllotaxisPosition.x, phyllotaxisPosition.y, 0));
+
+        Vector3 dotRotate = new Vector3(0f, 0f, dotRotateZ);
+        
+        //Spawns new dot
+        GameObject dotInstance = Instantiate(dot, positions[currentIndex], Quaternion.Euler(dotRotate), transform);
+            
+        dots.Add(dotInstance);
+            
+        dotInstance.transform.localScale = new Vector3(dotScale, dotScale, dotScale);
+
+        ColourPhyllotaxis(dotInstance);
+        
+        currentIndex++;
+    }
+
+    public void ColourPhyllotaxis(GameObject dot)
+    {
+        currentColour = Color.Lerp(currentColour, gradientColours[colourIndex], gradientLerpRate);
+
+        dot.GetComponentInChildren<MeshRenderer>().material.color = currentColour;
+
+        if (currentColour == gradientColours[colourIndex])
+        {
+            colourIndex = (colourIndex + 1) % gradientColours.Count;
+        }
+    }
+
+    private void ClearPhyllyotaxis()
+    {
+        foreach (var dot in dots)
+        {
+            Destroy(dot);
+        }
+        
+        dots.Clear();
+        positions.Clear();
+
+        currentIndex = 0;
+    }
+}
---
+++ Procedural Phyllotaxis/Assets/Scripts/TrailPhyllotaxis.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailPhyllotaxis : MonoBehaviour
+{
+    [Range(0f, 360f)]
+    public float degreeDelta;
+    public float scale;
+
+    public int maxIteration;
+    public int stepSize;
+    private int currentIteration;
+    
+    
+    public bool useLerp;
+    public float intervalLerp;
+    private bool isLerping;
+    private Vector3 startPosition, endPosition;
+    private float timeStartedLerp;
+    
+    public int numberStart;
+    private int number;
+
+    private TrailRenderer _trailRenderer;
+    
+    public int colourIndex;
+    public Color currentColour;
+    public List<Color> gradientColours = new List<Color>();
+
+    [Range(0f, 1f)]
+    public float gradientLerpRate;
+    
+    private Vector2 phyllotaxisPosition;
+
+    private List<Vector3> positions = new List<Vector3>();
+
+    private void Awake()
+    {
+        _trailRenderer = GetComponent<TrailRenderer>();
+        number = numberStart;
+        transform.localPosition = CalculatePhyllotaxis(degreeDelta, scale, number);
+
+        if (useLerp)
+        {
+            StartLerping();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClearPhyllyotaxis();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CreateNewPhyllotaxisPoint();
+        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ClearPhyllyotaxis();
+            
+            Awake();
+        }
+    }
+
+    void StartLerping()
+    {
+        isLerping = true;
+        timeStartedLerp = Time.time;
+        phyllotaxisPosition = CalculatePhyllotaxis(degreeDelta, scale, number);
+        startPosition = transform.localPosition;
+        endPosition = new Vector3(phyllotaxisPosition.x, phyllotaxisPosition.y, 0f);
+    }
+    
+    /// <summary>
+    /// Calculates the position of the dot using the phyllotaxis formula
+    /// </summary>
+    /// <param name="inDegreeDelta"></param>
+    /// The change in angle per step in degrees
+    /// <param name="inScale"></param>
+    /// <param name="inCount"></param>
+    /// The iteration into the pattern
+    /// <returns></returns>
+    private Vector2 CalculatePhyllotaxis(float inDegreeDelta, float inScale, int inCount)
+    {
+        double angle = inCount * (inDegreeDelta * Mathf.Deg2Rad);
+
+        float r = inScale * Mathf.Sqrt(inCount);
+
+        //Calculating the coordinate of the new position
+        float x = r * (float) System.Math.Cos(angle);
+        float y = r * (float) System.Math.Sin(angle);
+
+        Vector2 vec2 = new Vector2(x, y);
+        
+        return vec2;
+    }
+
+    void CreateNewPhyllotaxisPoint()
+    {
+        if (useLerp)
+        {
+            if (isLerping)
+            {
+                float timeSinceStarted = Time.time - timeStartedLerp;
+                float percentageComplete = timeSinceStarted / intervalLerp;
+                transform.localPosition = Vector3.Lerp(startPosition, endPosition, percentageComplete);
+
+                if (percentageComplete >= 0.97f)
+                {
+                    transform.localPosition = endPosition;
+                    number += stepSize;
+                    currentIteration++;
+
+                    if (currentIteration <= maxIteration)
+                    {
+                        StartLerping();
+                    }
+                    else
+                    {
+                        isLerping = false;
+                    }
+                }
+            }
+        }
+        else
+        {
+            //Calculates 2D position for new dot
+            phyllotaxisPosition = CalculatePhyllotaxis(degreeDelta, scale, number);
+
+            // //Creates and stores 3D position for new dot
+            // positions.Add(new Vector3(phyllotaxisPosition.x, phyllotaxisPosition.y, 0));
+            //
+            // transform.localPosition = positions[number];
+
+            transform.localPosition = new Vector3(phyllotaxisPosition.x, phyllotaxisPosition.y, 0);
+
+            number+= stepSize;
+            currentIteration++;
+        }
+    }
+
+    public void ColourPhyllotaxis(GameObject dot)
+    {
+        currentColour = Color.Lerp(currentColour, gradientColours[colourIndex], gradientLerpRate);
+
+        dot.GetComponentInChildren<MeshRenderer>().material.color = currentColour;
+
+        if (currentColour == gradientColours[colourIndex])
+        {
+            colourIndex = (colourIndex + 1) % gradientColours.Count;
+        }
+    }
+
+    private void ClearPhyllyotaxis()
+    {
+        positions.Clear();
+
+        currentIteration = 0;
+        
+        number = 0;
+        
+        _trailRenderer.Clear();
+
+        transform.localPosition = Vector3.zero;
+    }
+}
